2355f0dc37a48373a5e5d79cac2bc6cb4fd1c500
attacker145 <chakr1@yahoo.com>
2016-12-12 10:32:11 -0800

    Add files via upload

---
+++ Debug/USB_app/subdir_vars.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../USB_app/usbConstructs.c \
+../USB_app/usbEventHandling.c 
+
+OBJS += \
+./USB_app/usbConstructs.obj \
+./USB_app/usbEventHandling.obj 
+
+C_DEPS += \
+./USB_app/usbConstructs.d \
+./USB_app/usbEventHandling.d 
+
+C_DEPS__QUOTED += \
+"USB_app\usbConstructs.d" \
+"USB_app\usbEventHandling.d" 
+
+OBJS__QUOTED += \
+"USB_app\usbConstructs.obj" \
+"USB_app\usbEventHandling.obj" 
+
+C_SRCS__QUOTED += \
+"../USB_app/usbConstructs.c" \
+"../USB_app/usbEventHandling.c" 
+
+
---
+++ Debug/USB_app/usbConstructs.d
@@ -0,0 +1,27 @@
+# FIXED
+
+USB_app/usbConstructs.obj: ../USB_app/usbConstructs.c
+USB_app/usbConstructs.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/device.h
+USB_app/usbConstructs.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+USB_app/usbConstructs.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h
+USB_app/usbConstructs.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+USB_app/usbConstructs.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics.h
+USB_app/usbConstructs.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics_legacy_undefs.h
+USB_app/usbConstructs.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_config/descriptors.h
+USB_app/usbConstructs.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdint.h
+USB_app/usbConstructs.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/usb.h
+USB_app/usbConstructs.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_CDC_API/UsbCdc.h
+USB_app/usbConstructs.obj: ../USB_app/usbConstructs.h
+
+../USB_app/usbConstructs.c: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/device.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics_legacy_undefs.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_config/descriptors.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdint.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/usb.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_CDC_API/UsbCdc.h: 
+../USB_app/usbConstructs.h: 
