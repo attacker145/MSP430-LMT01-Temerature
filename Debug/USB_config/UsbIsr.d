f6fe4d6a43d3fb5d164d44239ad6d75ef9ab4f25
attacker145 <chakr1@yahoo.com>
2016-12-12 10:32:27 -0800

    Add files via upload

---
+++ Debug/USB_config/UsbIsr.d
@@ -0,0 +1,33 @@
+# FIXED
+
+USB_config/UsbIsr.obj: ../USB_config/UsbIsr.c
+USB_config/UsbIsr.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/device.h
+USB_config/UsbIsr.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+USB_config/UsbIsr.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h
+USB_config/UsbIsr.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+USB_config/UsbIsr.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics.h
+USB_config/UsbIsr.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics_legacy_undefs.h
+USB_config/UsbIsr.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/defMSP430USB.h
+USB_config/UsbIsr.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdint.h
+USB_config/UsbIsr.obj: ../USB_config/descriptors.h
+USB_config/UsbIsr.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/usb.h
+USB_config/UsbIsr.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/UsbIsr.h
+USB_config/UsbIsr.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/string.h
+USB_config/UsbIsr.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/linkage.h
+USB_config/UsbIsr.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_CDC_API/UsbCdc.h
+
+../USB_config/UsbIsr.c: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/device.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics_legacy_undefs.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/defMSP430USB.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdint.h: 
+../USB_config/descriptors.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/usb.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/UsbIsr.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/string.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/linkage.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_CDC_API/UsbCdc.h: 
---
+++ Debug/USB_config/descriptors.d
@@ -0,0 +1,29 @@
+# FIXED
+
+USB_config/descriptors.obj: ../USB_config/descriptors.c
+USB_config/descriptors.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/device.h
+USB_config/descriptors.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430.h
+USB_config/descriptors.obj: C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h
+USB_config/descriptors.obj: C:/ti/ccsv6/ccs_base/msp430/include/in430.h
+USB_config/descriptors.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics.h
+USB_config/descriptors.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics_legacy_undefs.h
+USB_config/descriptors.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/defMSP430USB.h
+USB_config/descriptors.obj: C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdint.h
+USB_config/descriptors.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/usb.h
+USB_config/descriptors.obj: ../USB_config/descriptors.h
+USB_config/descriptors.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_CDC_API/UsbCdc.h
+USB_config/descriptors.obj: E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_HID_API/UsbHidReq.h
+
+../USB_config/descriptors.c: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/device.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/msp430f5529.h: 
+C:/ti/ccsv6/ccs_base/msp430/include/in430.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/intrinsics_legacy_undefs.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/defMSP430USB.h: 
+C:/ti/ccsv6/tools/compiler/ti-cgt-msp430_16.6.0.STS/include/stdint.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_Common/usb.h: 
+../USB_config/descriptors.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_CDC_API/UsbCdc.h: 
+E:/Projects/CC3200\ Development/CCSworkspace_1120/C0_SimpleSend_with_CompB_Rev01/USB_API/USB_HID_API/UsbHidReq.h: 
